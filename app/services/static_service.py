b45b0c9ecb30cc235b7a4be652c818a887fd117a
kangminsu <112845890+kangmin5133@users.noreply.github.com>
2023-11-13 10:13:10 +0900

    Update static_service.py

--- app/services/static_service.py
+++ app/services/static_service.py
@@ -5,7 +5,7 @@ from app.db.mysql import crud, metadatas
 import shutil
 from tempfile import NamedTemporaryFile
 import datetime
-from datetime import timezone
+from datetime import timezone, timedelta
 import json
 from config.config import Config
 import zipfile
@@ -35,3 +35,49 @@ async def get_vehicle_count_by_type(db: Session):
     }
     
     return response
+
+async def get_sensor_data_count_by_window(window: str):
+    db = InfluxDatabase()
+    
+    start_time_str = (datetime.datetime.now() - timedelta(days=30)).isoformat().split(".")[0]+"Z"
+    start_time = datetime.datetime.fromisoformat(start_time_str)
+
+    stop_time_str = datetime.datetime.now().isoformat().split(".")[0]+"Z"
+    stop_time = datetime.datetime.fromisoformat(stop_time_str)
+
+    total_duration = stop_time - start_time
+    window_duration_seconds = parse_window_to_seconds(window)
+    total_index = int(total_duration.total_seconds() / window_duration_seconds)
+
+    count_by_window_query = f'''
+            from(bucket: "{db.bucket}")
+            |> range(start: {Config.DEFAULT_TIME_RANGE}, stop: {stop_time_str})
+            |> filter(fn: (r) => r._measurement == "{db.measurement}")
+            |> filter(fn: (r) => r._field == "timestamp")
+            |> window(every: {window})
+            |> count()
+            |> yield(name: "count")
+        '''
+    logging.info(f'Query for getting sensor data count by window: {count_by_window_query}')
+    result = db.query_data(count_by_window_query)
+
+    data_for_chart = [{"index": i, "id": "timestamp", "count": 0} for i in range(total_index)]
+
+    for table in result:
+        for record in table.records:
+            start_of_window = record.get_start()
+            index = int((start_of_window - start_time).total_seconds() / window_duration_seconds)
+            if 0 <= index < total_index:
+                data_for_chart[index]["count"] = record.get_value()
+
+    return data_for_chart
+
+def parse_window_to_seconds(window: str):
+    unit_to_seconds = {'h': 3600, 'd': 86400}
+    for unit in unit_to_seconds:
+        if window.endswith(unit):
+            return int(window.replace(unit, '')) * unit_to_seconds[unit]
+    raise ValueError(f"Invalid window format: {window}")
+
+
+
