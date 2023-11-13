639b3f6f28d5d11347f2ba83ef3c1624e450ae7a
kangminsu <112845890+kangmin5133@users.noreply.github.com>
2023-11-13 10:13:30 +0900

    Update static_api.py

--- app/api/v1/static_api.py
+++ app/api/v1/static_api.py
@@ -16,3 +16,11 @@ router = APIRouter()
 async def count_vehicles_by_vehicle_type(db : Session = Depends(get_db)):
     response = await static_service.get_vehicle_count_by_type(db)
     return JSONResponse(content=response) 
+
+@router.get("/count/data/by/window")
+async def get_sensor_data_count_by_window(window:str = Query("1h")):
+    if window is None:
+        raise HTTPException(status_code=400, detail="window is required in the params")
+    
+    result = await static_service.get_sensor_data_count_by_window(window = window)
+    return result
