82bbc3c90e071a0bdc43476b4c0c8c8fd5baa17b
kangminsu <112845890+kangmin5133@users.noreply.github.com>
2023-11-21 14:43:29 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
 import uvicorn
-from logging.handlers import TimedRotatingFileHandler
-from config.logger_config import setup_logger
-
 
 if __name__ == "__main__":
-    setup_logger()
     uvicorn.run("app.main:app", host="0.0.0.0", port=8820, reload=True)
